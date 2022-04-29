53f778eddf9f2c5970326b07bbeacb7f3082a388
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-04-29 15:09:05 +0100

    version war

---
+++ src/main/java/com/sip/ams/DevopsWarApplication.java
@@ -0,0 +1,25 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DevopsWarApplication extends SpringBootServletInitializer {
+	
+	@ResponseBody
+	@RequestMapping("/home")
+	public String info()
+	{
+		return"<h1 align=center> Merci GK, Devops c'est Top</h1>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsWarApplication.class, args);
+	}
+
+}
